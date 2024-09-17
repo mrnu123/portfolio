2a0861a420a75891c3aea897c8d2e7a8def3f45c
Anuchit Raksaphon <anuchit.nu567@gmail.com>
2024-09-17 19:22:25 +0700

    fix (switchLanguage) : change selected to defaultValue

--- src/app/components/switchLanguage.tsx
+++ src/app/components/switchLanguage.tsx
@@ -1,8 +1,16 @@
+import { useState } from "react";
+
 const SwitchLanguage = () => {
+    const [selectedLanguage, setSelectedLanguage] = useState("en");
+
+    const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+        setSelectedLanguage(e.target.value);
+        console.log("Language changed to: ", e.target.value);
+    }
     return <>
         <div className="col-lg-1 col-md-12">
-            <select className="form-select form-select-sm" >
-                <option value={"en"} selected >EN</option>
+            <select className="form-select form-select-sm" value={selectedLanguage} onChange={handleLanguageChange} >
+                <option value={"en"} defaultValue={'en'} >EN</option>
                 <option value={"th"} >TH</option>
             </select>
         </div>
