a086d72e4a4302d1d9c9600437b739ca616dd482
anuchit <anuchit.nu567@gmail.com>
2024-10-30 00:24:49 +0700

    feat : add switch language

--- src/app/components/switchLanguage.tsx
+++ src/app/components/switchLanguage.tsx
@@ -1,20 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import getCookie from "../../helper/getCookies";
 
 const SwitchLanguage = () => {
-    const [selectedLanguage, setSelectedLanguage] = useState("en");
+  const languageCookie = getCookie("selectedLanguage");
+  const [selectedLanguage, setSelectedLanguage] = useState(
+    languageCookie || "en"
+  );
+  const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+      setSelectedLanguage(e.target.value);
 
-    const handleLanguageChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-        setSelectedLanguage(e.target.value);
-        console.log("Language changed to: ", e.target.value);
-    }
-    return <>
-        <div className="col-lg-1 col-md-12">
-            <select className="form-select form-select-sm" value={selectedLanguage} onChange={handleLanguageChange} >
-                <option value={"en"} defaultValue={'en'} >EN</option>
-                <option value={"th"} >TH</option>
-            </select>
-        </div>
+    console.log("Language changed to: ", e.target.value);
+    document.cookie = `selectedLanguage=${e.target.value}`;
+    window.location.reload();
+  };
+  return (
+    <>
+      <div className="col-lg-1 col-md-12">
+        <select
+          className="form-select form-select-sm"
+          value={selectedLanguage}
+          onChange={handleLanguageChange}
+        >
+          <option value={"en"}>EN</option>
+          <option value={"th"}>TH</option>
+        </select>
+      </div>
     </>
-}
+  );
+};
 
 export default SwitchLanguage;
--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -1,10 +1,11 @@
-import {getRequestConfig} from 'next-intl/server';
+import { NextApiRequest } from 'next';
+import { getRequestConfig } from 'next-intl/server';
+import { cookies } from 'next/headers';
  
 export default getRequestConfig(async () => {
-  // Provide a static locale, fetch a user setting,
-  // read from `cookies()`, `headers()`, etc.
-  const locale = 'en';
- 
+  const locale = cookies().get('selectedLanguage')?.value || 'en';
+  console.log('Locale:', locale);
+  // console.log(locale)
   return {
     locale,
     messages: (await import(`../../messages/${locale}.json`)).default
